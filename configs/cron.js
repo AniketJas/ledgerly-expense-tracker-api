edc61577503989e368c733944e25563c95e7754b
Aniket Jas <aniket.jas20@gmail.com>
2026-06-25 02:26:43 +0530

    updated log

--- configs/cron.js
+++ configs/cron.js
@@ -5,10 +5,10 @@ import https from "https";
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
-      else console.log("GET request failed", res.statusCode);
+      if (res.statusCode === 200) console.log("[CRON JOB] GET request sent successfully ");
+      else console.log("[CRON JOB] GET request failed", res.statusCode);
     })
-    .on("error", (e) => console.error("Error while sending request", e));
+    .on("error", (e) => console.error("[CRON JOB] Error while sending request", e));
 });
 
 export default job;
